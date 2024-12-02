bd53771f5d9b266d2eebaa928ae99b1e0d06c6bf
Thiago Tolotti <thiagotolotti@gmail.com>
2024-12-02 18:42:21 -0300

    zod validates code (4 digit number)

--- src/routes/password.ts
+++ src/routes/password.ts
@@ -39,10 +39,16 @@ export default async function password(req: Request, res: Response) {
 }
 
 function validateData(body: unknown) {
-  // TODO: Make regex to validate code
+  const codeSchema = z
+    .union([
+      z.string().regex(/^\d{4}$/),
+      z.number().int().gte(1000).lte(9999), // Allow integer codes from 1000 to 9999
+    ])
+    .transform((value) => value.toString());
+
   const bodySchema = z
     .object({
-      code: z.string(),
+      code: codeSchema,
       password: passwordSchema,
       email: z.string().email(),
     })
